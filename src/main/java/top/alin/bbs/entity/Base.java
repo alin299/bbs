df6353a8644165a15baab7b227169bedacb5a6a1
alin <823301694@qq.com>
2020-05-13 16:58:34 +0800

    修复bug...

--- src/main/java/top/alin/bbs/entity/Base.java
+++ src/main/java/top/alin/bbs/entity/Base.java
@@ -3,9 +3,11 @@ package top.alin.bbs.entity;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.Data;
 
 import java.util.Date;
 
+@Data
 public class Base {
     @TableField(fill = FieldFill.INSERT)
     private Date createTime;
